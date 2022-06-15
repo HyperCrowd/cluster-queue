cc8d71e835de76578e4f374ccdd67789ab4d2bef
HyperCrowd <github@hypercrowd.org>
2022-06-15 04:19:25 -0500

    1 file modified

--- tests/primaryTest.ts
+++ tests/primaryTest.ts
@@ -2,44 +2,19 @@ import { Cluster } from '../src';
 import { test } from 'uvu';
 
 test('Test primary', async () => {
-  const instance = new Cluster(
-    [
-      {
-        command: 'dist',
-        action: () => undefined,
-      },
-      {
-        command: 'cli:setText',
-        description: 'Sets text in the primary process',
-        args: {
-          '<text>': 'The name of the state to set',
-        },
-        options: {
-          '-f': 'First character only',
-        },
-        action: async (command, state, sends) => {
-          state.text =
-            command.args.f === true
-              ? command.args.cli.text[0]
-              : command.args.cli.text;
+  const instance = new Cluster([
+    {
+      command: 'iterate',
+      action: async (command, state, sends) => {
+        if (state.value === undefined) {
+          state.value = 0;
+        }
 
-          console.log('setText', state);
-        },
+        state.value += 1;
+        console.log('Iterated value:', state.value);
       },
-      {
-        command: 'iterate',
-        action: async (command, state, sends) => {
-          if (state.value === undefined) {
-            state.value = 0;
-          }
-
-          state.value += 1;
-          console.log('Iterated value:', state.value);
-        },
-      },
-    ],
-    true
-  ).onCommand(
+    },
+  ]).onCommand(
     async (command, state, sends) => {
       console.info(`Primary Message: ${command.command}`);
     },
